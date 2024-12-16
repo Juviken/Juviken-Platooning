95e198d06c8bc5557e68dce6f617cbb26ca0e40c
Juviken <emiljuvikenhultman@gmail.com>
2024-12-16 12:47:22 +0100

    as

--- src/controller/src/controller/pid.py
+++ src/controller/src/controller/pid.py
@@ -1,5 +1,5 @@
 class PID:
-    def __init__(self, kp, ki, kd, dt: int, pid_min: float = None, pid_max: float = None, max_integral: float = None):
+    def __init__(self, kp, ki, kd, dt: int, pid_min: float = None, pid_max: float = None):
         self.error = 0
         self.error_integral = 0
         self.error_derivative = 0
@@ -10,26 +10,12 @@ class PID:
 
         self.pid_min = pid_min if pid_min is not None else -float('inf')
         self.pid_max = pid_max if pid_max is not None else float('inf')
-        
-        # Maximum allowable integral value, anti-windup
-        self.max_integral = max_integral if max_integral is not None else float('inf')
 
     def update(self, error: float):
         error_prev = self.error
         self.error = error
-        
-        # Update integral term with clamping
-        self.error_integral += error * self.dt
-        self.error_integral = max(min(self.error_integral, self.max_integral), -self.max_integral)
-        #lOG THE INTEGRAL in ros
-        
+        self.error_integral += error*self.dt
+        self.error_derivative = (error - error_prev)/self.dt
 
-        
-        # Update derivative term
-        self.error_derivative = (error - error_prev) / self.dt
-
-        # Compute the PID output
-        u = self.Kp * error + self.Ki * self.error_integral + self.Kd * self.error_derivative
-
-        # Constrain the output to allowable range
+        u = self.Kp*error + self.Ki*self.error_integral + self.Kd*self.error_derivative
         return min(max(u, self.pid_min), self.pid_max)
