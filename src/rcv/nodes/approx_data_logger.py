97e940f5497a126f418b8eb47a25c8f51ee6ddb2
Juviken <emiljuvikenhultman@gmail.com>
2024-12-21 14:59:34 +0100

    as

--- src/rcv/nodes/approx_data_logger.py
+++ src/rcv/nodes/approx_data_logger.py
@@ -3,53 +3,62 @@
 import rospy
 import message_filters
 from std_msgs.msg import Int32, Float32
+from sensor_msgs.msg import Joy
 
-class SynchronizedDataLogger:
+class ApproxDataLogger:
     def __init__(self):
         # Open the file for writing (CSV format)
         self.file = open("/tmp/vehicle_synchronized_data.csv", "w")
-        self.file.write("stamp,pwm,velocity,steering_angle\n")
+        # CSV header
+        self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
 
-        # Create message filters subscribers
+        # Create message_filters subscribers
         sub_pwm      = message_filters.Subscriber("/vehicle_0/pwm", Int32)
         sub_velocity = message_filters.Subscriber("/vehicle_0/velocity", Float32)
         sub_steering = message_filters.Subscriber("/vehicle_0/steering_angle", Int32)
+        sub_joy      = message_filters.Subscriber("/joy", Joy)
 
         # ApproximateTimeSynchronizer:
-        #  - queue_size controls how many messages are buffered.
-        #  - slop is the allowed delay (in seconds) between messages to still be synchronized.
-        #  - allow_headerless=True means messages without a std_msgs/Header are allowed.
-        ts = message_filters.ApproximateTimeSynchronizer(
-            [sub_pwm, sub_velocity, sub_steering],
+        #  - queue_size: number of messages buffered
+        #  - slop: allowed time difference (seconds)
+        #  - allow_headerless=True: because Int32/Float32 have no header
+        #    and Joy does have a header, we must allow mixing.
+        ats = message_filters.ApproximateTimeSynchronizer(
+            [sub_pwm, sub_velocity, sub_steering, sub_joy],
             queue_size=10,
             slop=0.1,
             allow_headerless=True
         )
 
-        # Register the common callback for synchronized messages
-        ts.registerCallback(self.sync_callback)
+        # Register the common callback
+        ats.registerCallback(self.sync_callback)
 
-    def sync_callback(self, pwm_msg, velocity_msg, steering_msg):
+    def sync_callback(self, pwm_msg, velocity_msg, steering_msg, joy_msg):
         """
-        This callback is triggered once the three messages align in time 
-        within the specified 'slop' range.
+        This callback is triggered once the four messages align in time
+        (within the 'slop' range).
         """
         now = rospy.Time.now().to_sec()
+
+        # Extract data
         pwm_val = pwm_msg.data
-        velocity_val = velocity_msg.data
-        steering_val = steering_msg.data
+        vel_val = velocity_msg.data
+        steer_val = steering_msg.data
+
+        # Convert Joy arrays to string
+        joy_axes_str    = str(list(joy_msg.axes))
+        joy_buttons_str = str(list(joy_msg.buttons))
 
         # Write a single line to the CSV
-        line = f"{now},{pwm_val},{velocity_val},{steering_val}\n"
+        line = f"{now},{pwm_val},{vel_val},{steer_val},{joy_axes_str},{joy_buttons_str}\n"
         self.file.write(line)
-        # Flush to ensure data is actually written out
         self.file.flush()
 
     def on_shutdown(self):
         self.file.close()
 
 if __name__ == "__main__":
-    rospy.init_node("synchronized_data_logger", anonymous=True)
-    logger = SynchronizedDataLogger()
+    rospy.init_node("approx_data_logger", anonymous=True)
+    logger = ApproxDataLogger()
     rospy.on_shutdown(logger.on_shutdown)
     rospy.spin()
--- src/rcv/nodes/immediate_data_logger.py
+++ src/rcv/nodes/immediate_data_logger.py
@@ -1,48 +1,75 @@
 #!/usr/bin/env python3
+
 import rospy
 from std_msgs.msg import Int32, Float32
+from sensor_msgs.msg import Joy
 
-class DataLogger:
+class ImmediateDataLogger:
     def __init__(self):
-        # You can open a file here for writing:
-        self.file = open("/tmp/vehicle_data.csv", "w")
-        self.file.write("stamp,pwm,velocity,steering_angle\n")
+        # Open file to write CSV data
+        self.file = open("/tmp/vehicle_immediate_data.csv", "w")
+        # CSV header: stamp, pwm, velocity, steering_angle, joy_axes, joy_buttons
+        self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
 
+        # Variables to store the latest data from each topic
         self.pwm = None
         self.velocity = None
-        self.steering = None
+        self.steering_angle = None
+        self.joy_axes = []
+        self.joy_buttons = []
 
+        # Set up subscribers
         rospy.Subscriber("/vehicle_0/pwm", Int32, self.callback_pwm)
         rospy.Subscriber("/vehicle_0/velocity", Float32, self.callback_velocity)
-        rospy.Subscriber("/vehicle_0/steering_angle", Int32, self.callback_steering_angle)
-        rospy.Subscriber("/vehicle_0/control", Int32, self.callback_pwm)
-
-        # Use a timer to periodically save data (e.g., 50 Hz).
-        rospy.Timer(rospy.Duration(0.02), self.periodic_write)
+        rospy.Subscriber("/vehicle_0/steering_angle", Int32, self.callback_steering)
+        rospy.Subscriber("/joy", Joy, self.callback_joy)
 
     def callback_pwm(self, msg):
         self.pwm = msg.data
+        self.log_data()
 
     def callback_velocity(self, msg):
         self.velocity = msg.data
+        self.log_data()
 
-    def callback_steering_angle(self, msg):
-        self.steering = msg.data
+    def callback_steering(self, msg):
+        self.steering_angle = msg.data
+        self.log_data()
 
-    def periodic_write(self, event):
-        # Current ROS time
-        now = rospy.Time.now().to_sec()  
-        # Only write if we have received data from all topics
-        if self.pwm is not None and self.velocity is not None and self.steering is not None:
-            line = f"{now},{self.pwm},{self.velocity},{self.steering}\n"
-            self.file.write(line)
+    def callback_joy(self, msg):
+        """
+        For logging, we can convert the arrays to strings.
+        Example: "[0.0, 1.0]" for axes, "[0,0,1,0]" for buttons
+        """
+        self.joy_axes = list(msg.axes)
+        self.joy_buttons = list(msg.buttons)
+        self.log_data()
+
+    def log_data(self):
+        """Immediately write the latest data to the CSV file."""
+        now = rospy.Time.now().to_sec()
+
+        # Use 'nan' or another placeholder if no data has been received yet
+        pwm_val       = self.pwm if self.pwm is not None else 'nan'
+        vel_val       = self.velocity if self.velocity is not None else 'nan'
+        steer_val     = self.steering_angle if self.steering_angle is not None else 'nan'
+
+        # Convert axes and buttons to string (or keep them as is if you prefer)
+        joy_axes_str    = str(self.joy_axes) if self.joy_axes else '[]'
+        joy_buttons_str = str(self.joy_buttons) if self.joy_buttons else '[]'
+
+        # Write a single line for the current update
+        line = f"{now},{pwm_val},{vel_val},{steer_val},{joy_axes_str},{joy_buttons_str}\n"
+        self.file.write(line)
+        # Flush to ensure data is actually written
+        self.file.flush()
 
     def on_shutdown(self):
-        # Close your file or do final cleanup
+        """Clean up before exiting."""
         self.file.close()
 
 if __name__ == "__main__":
-    rospy.init_node("data_logger")
-    logger = DataLogger()
+    rospy.init_node("immediate_data_logger", anonymous=True)
+    logger = ImmediateDataLogger()
     rospy.on_shutdown(logger.on_shutdown)
     rospy.spin()
