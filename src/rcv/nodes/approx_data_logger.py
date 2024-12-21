1b9e2ddc720cf5fba547e46cb71fae6e1d1d97a1
Juviken <emiljuvikenhultman@gmail.com>
2024-12-21 15:55:35 +0100

    asas

--- src/rcv/nodes/approx_data_logger.py
+++ src/rcv/nodes/approx_data_logger.py
@@ -2,60 +2,113 @@
 
 import rospy
 import message_filters
-from std_msgs.msg import Int32, Float32
+from std_msgs.msg import Int32, Float32, Bool
 from sensor_msgs.msg import Joy
 
 class ApproxDataLogger:
     def __init__(self):
-        # Open the file for writing (CSV format)
-        self.file = open("/tmp/vehicle_synchronized_data.csv", "w")
-        # CSV header
-        self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
+        # Fetch parameters
+        self.vehicle_id   = rospy.get_param("VEHICLE_ID", "vehicle_0")
+        self.follower_car = rospy.get_param("FOLLOWER_CAR", False)
 
-        # Create message_filters subscribers
-        sub_pwm      = message_filters.Subscriber("/vehicle_0/pwm", Int32)
-        sub_velocity = message_filters.Subscriber("/vehicle_0/velocity", Float32)
-        sub_steering = message_filters.Subscriber("/vehicle_0/steering_angle", Int32)
-        sub_joy      = message_filters.Subscriber("/joy", Joy)
+        # Build filename
+        filename = f"/tmp/{self.vehicle_id}_synchronized_data.csv"
+        rospy.loginfo(f"[ApproxDataLogger] Logging to {filename}")
+        self.file = open(filename, "w")
 
-        # ApproximateTimeSynchronizer:
-        #  - queue_size: number of messages buffered
-        #  - slop: allowed time difference (seconds)
-        #  - allow_headerless=True: because Int32/Float32 have no header
-        #    and Joy does have a header, we must allow mixing.
-        ats = message_filters.ApproximateTimeSynchronizer(
-            [sub_pwm, sub_velocity, sub_steering, sub_joy],
-            queue_size=10,
-            slop=0.1,
-            allow_headerless=True
-        )
+        # Prepare the subscriber list for message_filters
+        # Common subscribers: pwm, velocity, steering
+        sub_pwm      = message_filters.Subscriber(f"/{self.vehicle_id}/pwm",            Int32)
+        sub_velocity = message_filters.Subscriber(f"/{self.vehicle_id}/velocity",       Float32)
+        sub_steering = message_filters.Subscriber(f"/{self.vehicle_id}/steering_angle", Int32)
 
-        # Register the common callback
-        ats.registerCallback(self.sync_callback)
+        # We'll store them in a list that we'll pass to the synchronizer
+        subscribers_list = [sub_pwm, sub_velocity, sub_steering]
 
-    def sync_callback(self, pwm_msg, velocity_msg, steering_msg, joy_msg):
-        """
-        This callback is triggered once the four messages align in time
-        (within the 'slop' range).
-        """
+        if not self.follower_car:
+            # Not follower -> also track /joy
+            # Write CSV header accordingly
+            self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
+
+            sub_joy = message_filters.Subscriber("/joy", Joy)
+            subscribers_list.append(sub_joy)
+
+            # Create ATS with 4 topics
+            self.sync = message_filters.ApproximateTimeSynchronizer(
+                subscribers_list,
+                queue_size=10,
+                slop=0.1,
+                allow_headerless=True  # needed since pwm/velocity/steering are headerless
+            )
+            self.sync.registerCallback(self.callback_not_follower)
+
+        else:
+            # FOLLOWER_CAR == True -> track distance, has_target, target_center_offset, control
+            # Write CSV header
+            self.file.write(
+                "stamp,pwm,velocity,steering_angle,distance,has_target,"
+                "target_center_offset,control\n"
+            )
+
+            sub_distance  = message_filters.Subscriber(f"/{self.vehicle_id}/distance",             Float32)
+            sub_has_target= message_filters.Subscriber(f"/{self.vehicle_id}/has_target",           Bool)
+            sub_offset    = message_filters.Subscriber(f"/{self.vehicle_id}/target_center_offset", Float32)
+            sub_control   = message_filters.Subscriber(f"/{self.vehicle_id}/control",              Int32)
+
+            # Add them to the list, so we have 7 topics total
+            subscribers_list.extend([sub_distance, sub_has_target, sub_offset, sub_control])
+
+            # Create ATS with 7 topics
+            self.sync = message_filters.ApproximateTimeSynchronizer(
+                subscribers_list,
+                queue_size=10,
+                slop=0.1,
+                allow_headerless=True
+            )
+            self.sync.registerCallback(self.callback_follower)
+
+    # ------------------------------------------------------
+    #  Callbacks for Not-Follower and Follower
+    # ------------------------------------------------------
+
+    def callback_not_follower(self, pwm_msg, velocity_msg, steering_msg, joy_msg):
+        """Approx. sync callback when not a follower."""
         now = rospy.Time.now().to_sec()
 
-        # Extract data
-        pwm_val = pwm_msg.data
-        vel_val = velocity_msg.data
-        steer_val = steering_msg.data
+        pwm_val    = pwm_msg.data
+        vel_val    = velocity_msg.data
+        steer_val  = steering_msg.data
 
-        # Convert Joy arrays to string
         joy_axes_str    = str(list(joy_msg.axes))
         joy_buttons_str = str(list(joy_msg.buttons))
 
-        # Write a single line to the CSV
         line = f"{now},{pwm_val},{vel_val},{steer_val},{joy_axes_str},{joy_buttons_str}\n"
+        self.file.write(line)
+        self.file.flush()
+
+    def callback_follower(self, pwm_msg, velocity_msg, steering_msg,
+                          distance_msg, has_target_msg, offset_msg, control_msg):
+        """Approx. sync callback when FOLLOWER_CAR == True."""
+        now = rospy.Time.now().to_sec()
+
+        pwm_val      = pwm_msg.data
+        vel_val      = velocity_msg.data
+        steer_val    = steering_msg.data
+        distance_val = distance_msg.data
+        has_target_val = has_target_msg.data
+        offset_val     = offset_msg.data
+        control_val    = control_msg.data
+
+        line = (
+            f"{now},{pwm_val},{vel_val},{steer_val},"
+            f"{distance_val},{has_target_val},{offset_val},{control_val}\n"
+        )
         self.file.write(line)
         self.file.flush()
 
     def on_shutdown(self):
         self.file.close()
+
 
 if __name__ == "__main__":
     rospy.init_node("approx_data_logger", anonymous=True)
--- src/rcv/nodes/immediate_data_logger.py
+++ src/rcv/nodes/immediate_data_logger.py
@@ -1,29 +1,65 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Int32, Float32
+from std_msgs.msg import Int32, Float32, Bool
 from sensor_msgs.msg import Joy
+
 
 class ImmediateDataLogger:
     def __init__(self):
-        # Open file to write CSV data
-        self.file = open("/tmp/vehicle_immediate_data.csv", "w")
-        # CSV header: stamp, pwm, velocity, steering_angle, joy_axes, joy_buttons
-        self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
+        # Fetch parameters
+        self.vehicle_id = rospy.get_param("VEHICLE_ID", "vehicle_0")
+        self.follower_car = rospy.get_param("FOLLOWER_CAR", False)
 
-        # Variables to store the latest data from each topic
+        # Build filename from vehicle ID
+        filename = f"/tmp/{self.vehicle_id}_immediate_data.csv"
+        rospy.loginfo(f"[ImmediateDataLogger] Logging to {filename}")
+        self.file = open(filename, "w")
+
+        # Common fields
+        #   stamp, pwm, velocity, steering_angle ...
+        # Then branch for joy OR distance/has_target/offset/control
+        if not self.follower_car:
+            # NOT a follower, record Joy
+            self.file.write("stamp,pwm,velocity,steering_angle,joy_axes,joy_buttons\n")
+        else:
+            # FOLLOWER_CAR == True, record follower topics
+            self.file.write(
+                "stamp,pwm,velocity,steering_angle,distance,has_target,"
+                "target_center_offset,control\n"
+            )
+
+        # Store latest data
         self.pwm = None
         self.velocity = None
         self.steering_angle = None
+
+        # Joy-specific
         self.joy_axes = []
         self.joy_buttons = []
 
-        # Set up subscribers
-        rospy.Subscriber("/vehicle_0/pwm", Int32, self.callback_pwm)
-        rospy.Subscriber("/vehicle_0/velocity", Float32, self.callback_velocity)
-        rospy.Subscriber("/vehicle_0/steering_angle", Int32, self.callback_steering)
-        rospy.Subscriber("/joy", Joy, self.callback_joy)
+        # Follower-specific
+        self.distance = None
+        self.has_target = None
+        self.target_center_offset = None
+        self.control = None
 
+        # Subscribe to common vehicle topics
+        rospy.Subscriber(f"/{self.vehicle_id}/pwm",            Int32,   self.callback_pwm)
+        rospy.Subscriber(f"/{self.vehicle_id}/velocity",       Float32, self.callback_velocity)
+        rospy.Subscriber(f"/{self.vehicle_id}/steering_angle", Int32,   self.callback_steering)
+
+        # If not follower, subscribe to /joy
+        if not self.follower_car:
+            rospy.Subscriber("/joy", Joy, self.callback_joy)
+        else:
+            # Subscribe to follower-specific topics
+            rospy.Subscriber(f"/{self.vehicle_id}/distance",              Float32, self.callback_distance)
+            rospy.Subscriber(f"/{self.vehicle_id}/has_target",            Bool,    self.callback_has_target)
+            rospy.Subscriber(f"/{self.vehicle_id}/target_center_offset",  Float32, self.callback_target_center_offset)
+            rospy.Subscriber(f"/{self.vehicle_id}/control",               Int32,   self.callback_control)
+
+    # ------------------- Callbacks ------------------- #
     def callback_pwm(self, msg):
         self.pwm = msg.data
         self.log_data()
@@ -36,37 +72,64 @@ class ImmediateDataLogger:
         self.steering_angle = msg.data
         self.log_data()
 
+    # Joy Callbacks (only if not follower)
     def callback_joy(self, msg):
-        """
-        For logging, we can convert the arrays to strings.
-        Example: "[0.0, 1.0]" for axes, "[0,0,1,0]" for buttons
-        """
         self.joy_axes = list(msg.axes)
         self.joy_buttons = list(msg.buttons)
         self.log_data()
 
+    # Follower Callbacks (only if FOLLOWER_CAR == True)
+    def callback_distance(self, msg):
+        self.distance = msg.data
+        self.log_data()
+
+    def callback_has_target(self, msg):
+        self.has_target = msg.data
+        self.log_data()
+
+    def callback_target_center_offset(self, msg):
+        self.target_center_offset = msg.data
+        self.log_data()
+
+    def callback_control(self, msg):
+        self.control = msg.data
+        self.log_data()
+
+    # ------------------- Logging ------------------- #
     def log_data(self):
-        """Immediately write the latest data to the CSV file."""
+        """Write the latest data to the CSV file immediately."""
         now = rospy.Time.now().to_sec()
 
-        # Use 'nan' or another placeholder if no data has been received yet
-        pwm_val       = self.pwm if self.pwm is not None else 'nan'
-        vel_val       = self.velocity if self.velocity is not None else 'nan'
-        steer_val     = self.steering_angle if self.steering_angle is not None else 'nan'
+        # Common fields
+        pwm_val    = self.pwm if self.pwm is not None else 'nan'
+        vel_val    = self.velocity if self.velocity is not None else 'nan'
+        steer_val  = self.steering_angle if self.steering_angle is not None else 'nan'
 
-        # Convert axes and buttons to string (or keep them as is if you prefer)
-        joy_axes_str    = str(self.joy_axes) if self.joy_axes else '[]'
-        joy_buttons_str = str(self.joy_buttons) if self.joy_buttons else '[]'
+        # Branch depending on follower or not
+        if not self.follower_car:
+            # Joy scenario
+            joy_axes_str    = str(self.joy_axes) if self.joy_axes else '[]'
+            joy_buttons_str = str(self.joy_buttons) if self.joy_buttons else '[]'
 
-        # Write a single line for the current update
-        line = f"{now},{pwm_val},{vel_val},{steer_val},{joy_axes_str},{joy_buttons_str}\n"
+            line = f"{now},{pwm_val},{vel_val},{steer_val},{joy_axes_str},{joy_buttons_str}\n"
+        else:
+            # Follower scenario
+            distance_val     = self.distance if self.distance is not None else 'nan'
+            has_target_val   = self.has_target if self.has_target is not None else 'nan'
+            offset_val       = self.target_center_offset if self.target_center_offset is not None else 'nan'
+            control_val      = self.control if self.control is not None else 'nan'
+
+            line = (
+                f"{now},{pwm_val},{vel_val},{steer_val},"
+                f"{distance_val},{has_target_val},{offset_val},{control_val}\n"
+            )
+
         self.file.write(line)
-        # Flush to ensure data is actually written
         self.file.flush()
 
     def on_shutdown(self):
-        """Clean up before exiting."""
         self.file.close()
+
 
 if __name__ == "__main__":
     rospy.init_node("immediate_data_logger", anonymous=True)
